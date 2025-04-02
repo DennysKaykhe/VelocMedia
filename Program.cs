dabf8dc44974ad9d099e3d25b005fda8c96e0845
Dennys Kaykhe Justino de Assis <Dennyssenpai@gmail.com>
2025-04-02 10:58:53 -0300

    Eu amo velocidade média

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double distancia;
+double tempo;
+double media;
+
+Console.Clear();
+
+Console.Write("Distância percorrida (m): ");
+distancia = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Tempo gasto (s).........: ");
+tempo = Convert.ToDouble(Console.ReadLine());
+
+media = distancia / tempo;
+
+Console.WriteLine(@$"
+Velocidade média: {media} m/s");
